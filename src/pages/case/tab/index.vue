77db490add98522be8d5e4e92b17c99012f0c6d1
bo.wang002 <bo.wang002@daddylab.com>
2024-03-25 10:43:07 +0800

    feat: tab案例

--- src/pages/case/tab/index.vue
+++ src/pages/case/tab/index.vue
@@ -19,12 +19,13 @@
 <template>
   <div class="mb14px header_title">演示非同源新tab标签消息通知</div>
 
-  <h2>将非同源页面通过ifrmae转为父级同源， 新tab页面postMessage通信，同源页面bus通信</h2>
+  <p>
+    1、场景：公司不同项目下a项目的商品详情，同样适用于b项目，不想二次开发。所以b项目引入了a商品详情。
+    a项目下的商品详情有改动后希望通知b。b项目接到通知可刷新列表。b项目下新建路由iframe套入a项目的商品详情地址。a,b在iframe中可以使用postMessage通信。然后b在做bus通知其他具体的页面刷新
+  </p>
 
   <Codemirror
     v-model:value="code"
     border
-    height="400"
-    width="600"
   />
 </template>
